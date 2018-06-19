efaae9f61f984fc45b1f152307d31d9d01ea4d2e
Cleiton <cleiton03122000@gmail.com>
2018-06-19 10:47:20 -0300

    Cleiton arrumou o erro do build e apagou o classe customListViewAlfabeto por culpa do elton bater nele
    Signed-off-by: aluno <eltonlemoscardozo@gmail.com>

--- app/src/main/java/com/example/elton/libras/Tela3.java
+++ app/src/main/java/com/example/elton/libras/Tela3.java
@@ -1,8 +1,8 @@
 package com.example.elton.libras;
 
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 
@@ -10,6 +10,8 @@ public class Tela3 extends AppCompatActivity {
 
     private Button button4;
     private Intent intent;
+    private Button button2;
+    private Intent intent2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,14 +19,23 @@ public class Tela3 extends AppCompatActivity {
         setContentView(R.layout.activity_tela3);
 
         intent = new Intent(this,Alfabeto.class);
+        intent2 = new Intent(this, Teste.class);
 
-        button4 = (Button)findViewById(R.id.button4);
+        button4 = findViewById(R.id.button4);
         button4.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 startActivity(intent);
             }
         });
+
+        button2 = findViewById(R.id.button2);
+        button2.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View view) {
+                startActivity(intent2);
+            }
+        });
+
 
     }
 }
